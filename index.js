69ccbad68fc4844cbd3bd414d84684752d4c1daf
Elsie Urena <71279445+elsieu9700@users.noreply.github.com>
2023-06-26 12:33:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function onClick(element) {
 }
 
 // Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction();};
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -20,9 +20,40 @@ function myFunction() {
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
     var x = document.getElementById("navDemo");
-    if (x.className.indexOf("show") == -1) {
+    if (x.className.indexOf("show") === -1) {
         x.className += "show";
     } else {
         x.className = x.className.replace("show", "");
     }
+}
+
+// Scroll to the top button also doubles as a return home button for main page
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Navigation bar make it shrink for responsive website
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
